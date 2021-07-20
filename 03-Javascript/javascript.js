aa1dd2fd33548f829d752b6e6ac7fcf697d9483b
Charlotte France <charlie.france@ntlworld.com>
2021-07-20 19:57:15 +0100

    done first code along

---
+++ 03-Javascript/javascript.js
@@ -0,0 +1,33 @@
+const name = "Charlotte";
+if (name == "Charlotte") {
+    console.log("its Charlotte!")
+}
+
+const greeting = "bonjour";
+if (greeting == "bonjour") {
+    console.log("comment ca va")
+}
+
+const score = 10;
+if (score == "10") {
+    console.log("Congratulations! you won!")
+}
+
+let age = 50;
+if (age >= 65) {
+    console.log("Time to retire!")
+}
+
+const hasCovid = false;
+const isShielding = true;
+if (hasCovid || isShielding) {
+    console.log("time to isolate")
+} else {
+    console.log("Keep social distancing!")
+}
+
+const isBritish = true;
+age = 18
+if (age > 17 && isBritish) {
+    console.log("Have you registered to vote?")
+}
