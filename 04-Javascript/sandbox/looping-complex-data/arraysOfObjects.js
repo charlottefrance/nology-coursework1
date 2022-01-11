8d725c8bcd2b29fe00b6a5b183df0b688fb5dcd1
Charlotte France <charlie.france@ntlworld.com>
2022-01-11 18:12:11 +0000

    done for today

--- 04-Javascript/sandbox/looping-complex-data/arraysOfObjects.js
+++ 04-Javascript/sandbox/looping-complex-data/arraysOfObjects.js
@@ -1,44 +1,69 @@
+// const fruitObjectArray = [
+//     { fruit: "apple" },
+//     { fruit: "banana" },
+//     { fruit: "orange" }
+// ];
+
+// // third challenge
+
+// // solution is the same as what i did :D
+// // console.log(fruitObjectArray[1].fruit);
+
+
+// // console.log(fruitObjectArray[0].fruit);
+// // console.log(fruitObjectArray[1].fruit);
+// // console.log(fruitObjectArray[2].fruit);
+
+// // Iterating over an array -> array of the values
+
+// const fruitValueArray = fruitObjectArray.map((fruitObject) => {
+//     const fruitValue = fruitObject.fruit;
+//     return fruitValue;
+// })
+
+// // console.log(fruitValueArray);
+
+// const searchTerm = "e";
+
+// const matchingFruitArray = fruitObjectArray.filter((fruitObject) => {
+//     const fruitMatch = fruitObject.fruit.includes(searchTerm);
+//     return fruitMatch;
+// })
+
+// console.log(matchingFruitArray);
+
+// //fourth challenge
+// // couldnt figure it out
+
+// //SOLUTION
+// const generateFruitHTML = (fruitArray) => {
+//     const html = fruitArray.map(fruitObject => {
+//         return `<li>${fruitObject.fruit}</li>`
+//     });
+    
+//     return html.join("");
+// };
+
+// console.log(generateFruitHTML(fruitObjectArray));
+
 const fruitObjectArray = [
-    { fruit: "apple" },
-    { fruit: "banana" },
-    { fruit: "orange" }
+    { fruit: "apple" , rating: 8, price: 150 },
+    { fruit: "banana" , rating: 5, price: 100 },
+    { fruit: "orange" , rating: 6, price: 120 }
 ];
 
-// third challenge
+// console.log(fruitObjectArray[2].price)
 
-// solution is the same as what i did :D
-// console.log(fruitObjectArray[1].fruit);
-
-
-// console.log(fruitObjectArray[0].fruit);
-// console.log(fruitObjectArray[1].fruit);
-// console.log(fruitObjectArray[2].fruit);
-
-// Iterating over an array -> array of the values
-
-const fruitValueArray = fruitObjectArray.map((fruitObject) => {
-    const fruitValue = fruitObject.fruit;
-    return fruitValue;
-})
-
-// console.log(fruitValueArray);
-
-const searchTerm = "e";
-
-const matchingFruitArray = fruitObjectArray.filter((fruitObject) => {
-    const fruitMatch = fruitObject.fruit.includes(searchTerm);
-    return fruitMatch;
-})
-
-console.log(matchingFruitArray);
-
-//fourth challenge
-// couldnt figure it out
-
-//SOLUTION
 const generateFruitHTML = (fruitArray) => {
     const html = fruitArray.map(fruitObject => {
-        return `<li>${fruitObject.fruit}</li>`
+        const cardHtml = `
+        <div>
+            <h3> ${fruitObject.fruit} </h3>
+            <p>This fruit is ${fruitObject.rating} / 10 </p>
+            <p>Buy now for ${fruitObject.price}p</p>
+        </div>
+        `
+        return cardHtml;
     });
     
     return html.join("");
