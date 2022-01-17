985a53b0eaaa81eccaab9626312a19c379c35ecc
Charlotte France <charlie.france@ntlworld.com>
2022-01-17 19:03:30 +0000

    started jest lesson

--- 04-Javascript/jest-testing/index.test.js
+++ 04-Javascript/jest-testing/index.test.js
@@ -1,6 +1,11 @@
 import {getName} from "./index.js";
 
-it("Should return the string 'Charlotte, 21 years old.'", () => {
+it("Should return the string 'Charlotte, 21 years old'", () => {
     const output = getName('Charlotte', 21);
     expect(output).toBe('Charlotte, 21 years old');
+})
+
+it("Should return the string 'Luke, 22 years old'", () => {
+    const output = getName('Luke', 22);
+    expect(output).toBe('Luke, 22 years old');
 })
