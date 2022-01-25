25b058dab2c0139b9af6dd671f3a88f8345482c0
Charlotte France <charlie.france@ntlworld.com>
2022-01-25 13:14:03 +0000

    intro done

--- 04-Javascript/promises/app.js
+++ 04-Javascript/promises/app.js
@@ -1,1 +1,19 @@
-console.log("working");
+// A promise is a way of waiting for data
+// A promise has 3 states, pending, resolved and rejected
+
+let samsPromise = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a == 2) {
+        resolve("Success")
+    } else {
+        reject("Failed")
+    }
+})
+
+samsPromise
+    .then((message) => {
+        console.log(message + `, I am in the THEN`)
+    })
+    .catch((message) => {
+        console.log(message + `, I am in the CATCH`)
+    });
