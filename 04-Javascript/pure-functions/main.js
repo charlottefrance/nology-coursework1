efd347bf30430918ae934831d0344ef0e6cd1e1a
Charlotte France <charlie.france@ntlworld.com>
2022-01-25 10:55:18 +0000

    finsing pure-functions lesson

--- 04-Javascript/pure-functions/main.js
+++ 04-Javascript/pure-functions/main.js
@@ -33,42 +33,30 @@
 
 
 // https://gist.github.com/mbickell/f613fc2bf3095ca91830d40df4ebffa5
-const testForTechKeyword = (arrayName) => {
-  let myArray = [];
-  arrayName.forEach((phrase) => {
-    if (phrase.includes("tech")) {
-      return myArray.push(phrase);
-    } else if (phrase.includes("Tech")) {
-        return myArray.push(phrase);
-    } else {
-        return "nope not tech"
-    }
-  });
-  return myArray;
-};
 
-const capitalisingFullName = (firstName, lastName) => {
-    const fullName = firstName.toUpperCase() + " " + lastName.toUpperCase();
-    return fullName;
-};
+const capitalisingFullName = (firstName, lastName) => `${firstName.toUpperCase()} ${lastName.toUpperCase()}`;
 
 const findsDaysSinceBirth = (birthday) => {
-    const bday = new Date(birthday);
-    const aDate = new Date("02/16/2021");
-    const diff = aDate.getTime() - bday.getTime();
+    const userBirthday = new Date(birthday);
+    const todaysDate = new Date();
 
-    let ms = 1000 * 3600 * 24;
+    const differenceInMS = todaysDate.getTime() - userBirthday.getTime();
+    const MSInADay = 1000 * 3600 * 24;
 
-    const result = diff / ms;
-    return result;
+    const numberOfDaysSinceBirth = Math.floor(differenceInMS / MSInADay);
+    return numberOfDaysSinceBirth;
 };
 
+const testForTechKeyword = (phraseArray) => {
+    const techSearchInPhrase = phraseArray.filter((phrase) => phrase.toLowerCase().includes("tech"));
+    return techSearchInPhrase;
+  };
+
+console.log(capitalisingFullName("matt", "bickell"));
+console.log(findsDaysSinceBirth("09/15/2000"))
 console.log(testForTechKeyword([
     "Tech is booming",
     "Covid 19 yada yada yada",
     "Economy due for a resurgance!",
     "You can retrain in tech too!"
   ]));
-
-console.log(capitalisingFullName("matt", "bickell"));
-console.log(findsDaysSinceBirth("09/26/1993"))
