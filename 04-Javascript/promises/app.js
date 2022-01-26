6b1067b28b5f4677a61736c1d791054553ad3f2b
Charlotte France <charlie.france@ntlworld.com>
2022-01-26 11:37:41 +0000

    starting the challenge

--- 04-Javascript/promises/app.js
+++ 04-Javascript/promises/app.js
@@ -1,19 +1,42 @@
 // A promise is a way of waiting for data
 // A promise has 3 states, pending, resolved and rejected
 
-let samsPromise = new Promise((resolve, reject) => {
-    let a = 1 + 1;
-    if (a == 2) {
-        resolve("Success")
-    } else {
-        reject("Failed")
-    }
-})
+// let samsPromise = new Promise((resolve, reject) => {
+//     let a = 1 + 1;
+//     if (a == 2) {
+//         resolve("Success")
+//     } else {
+//         reject("Failed")
+//     }
+// })
 
-samsPromise
-    .then((message) => {
-        console.log(message + `, I am in the THEN`)
-    })
-    .catch((message) => {
-        console.log(message + `, I am in the CATCH`)
-    });
+// samsPromise
+//     .then((message) => {
+//         console.log(message + `, I am in the THEN`)
+//     })
+//     .catch((message) => {
+//         console.log(message + `, I am in the CATCH`)
+//     });
+
+
+// https://randomuser.me/api/
+// const heading = document.querySelector(".json-results")
+// const getRandomUser = () => {
+//     fetch('https://randomuser.me/api/')
+//         .then(res => res.json())
+//         .then(json => heading.innerHTML = json.results[0].name.first)
+//         .catch(err => console.log(err))
+// }
+
+// getRandomUser();
+
+// https://api.punkapi.com/v2/beers/random
+
+const getRandomBeer = () => {
+    fetch('https://api.punkapi.com/v2/beers/random')
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err))
+}
+
+getRandomBeer();
