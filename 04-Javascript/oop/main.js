f6835a85c739baf04e570ddd9f846f0be15d5b9b
Charlotte France <charlie.france@ntlworld.com>
2022-01-28 19:28:36 +0000

    started OOP

---
+++ 04-Javascript/oop/main.js
@@ -0,0 +1,9 @@
+class Book {
+    constructor(title, author, genre, year) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+    }
+
+}
